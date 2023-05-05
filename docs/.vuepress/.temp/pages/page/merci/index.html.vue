2512deed971cbe0e5ddd68cef571925c63e29948
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-05-05 19:35:37 +0200

    remove .temp

--- docs/.vuepress/.temp/pages/page/merci/index.html.vue
+++
@@ -1,8 +0,0 @@
-<template><div><h2 id="un-grand-merci" tabindex="-1"><a class="header-anchor" href="#un-grand-merci" aria-hidden="true">#</a> Un grand merci</h2>
-<p>Votre achat est important pour nous.</p>
-<p>Votre commande va être préparée dans les plus brefs délais.</p>
-<p>💖💖💖</p>
-<p><RouterLink to="/">Revenir à l'accueil</RouterLink></p>
-</div></template>
-
-
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-const version = '1.0.51';
+const version = '1.0.61';
 
 import { defineUserConfig } from '@vuepress/cli';
 import { viteBundler } from '@vuepress/bundler-vite';
